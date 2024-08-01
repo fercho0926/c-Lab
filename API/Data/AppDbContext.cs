d0121135b5c257965a7b3494dcadcc4af4dbec61
Milton Vasquez <milton.vasquez@kineticadvantage.com>
2024-08-01 07:05:01 -0500

    initial

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+
+
+
+}
